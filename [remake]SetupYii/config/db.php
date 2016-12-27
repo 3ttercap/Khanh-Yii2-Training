4676dc3af9a9f5b45a21fe502a60b46d79360c55
kahntran <hello.world.5192@gmail.com>
2016-12-27 17:32:04 +0700

    Remake Yii2 Framework From Scratch
    Pause at Active Record

---
+++ [remake]SetupYii/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii-basic',
+    'username' => 'root',
+    'password' => 'root123',
+    'charset' => 'utf8',
+];
---
+++ [remake]SetupYii/views/site/say.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KahnTran
+ * Date: 12/27/2016
+ * Time: 3:10 PM
+ */
+
+use yii\helpers\Html;
+?>
+
+<h1>Hello <?= Html::encode($target) ?></h1>
+<p>Welcome to your Yii2 demonstration application.</p>
